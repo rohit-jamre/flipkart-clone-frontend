9a434cac7b22ecae50c94555d60b9f8009d1379a
Rohit Jamre <jamrer193@gmail.com>
2024-02-25 20:28:56 +0530

    intial commit

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,17 @@
+
+
+import {AppBar, Toolbar, styled} from '@mui/material';
+const StyledHeader=styled(AppBar)
+'
+background: #2874f0;
+'
+const Header =() => {
+    return (
+        <StyledHeader>
+            <Toolbar>
+
+            </Toolbar>
+        </StyledHeader>
+    )
+}
+export default Header;
