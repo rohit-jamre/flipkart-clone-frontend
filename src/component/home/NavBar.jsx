75c30003f028c1dc914b4f4b5c892e8619c33f30
Rohit Jamre <jamrer193@gmail.com>
2024-02-29 19:37:49 +0530

    demo

--- src/component/home/NavBar.jsx
+++ src/component/home/NavBar.jsx
@@ -6,6 +6,9 @@ const Component = styled(Box)`
 display:flex;
 margin:55px 130px 0 130px;
 justify-content:space-between;
+`;
+const Container = stlyled(Box)`
+padding:12px 8px;
 `
 const NavBar = () => {
     return (
@@ -13,10 +16,10 @@ const NavBar = () => {
             {
                 navData.map(data =>
                     (
-                        <Box>
+                        <Container>
                             <img src = {data.url} alt ="nav" style={{width:64}}/>
                             <p>{data.text}</p>
-                        </Box>
+                        </Container>
                     )
                     )
             }
