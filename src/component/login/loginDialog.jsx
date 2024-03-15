265b29b1e056163ce2ba0f5b663ae431f00898f2
Rohit Jamre <jamrer193@gmail.com>
2024-03-15 19:08:45 +0530

    modified fornt end

--- src/component/login/loginDialog.jsx
+++ src/component/login/loginDialog.jsx
@@ -1,128 +1,146 @@
-import {useState} from 'react';
-import {Dialog,Box,TextField,Typography,Button,styled} from '@mui/material';
-import {authenticateSignup} from '../../service/api';
-const Component =styled(Box)`
-height:70vh;
-width:90vh;
-`;
-const Image = styled(Box)`
-background:#2874f0 url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png)center 85% no-repeat;
-height:83%;
-width:25%;
-padding:45px 35px;
-& > p, &> h5{
-  color:#FFFFFF;
-  font-weight:600;
-}
 
+import { useState } from 'react';
+import { Dialog, Box, TextField, Typography, Button, styled } from '@mui/material';
+import { authenticateSignup } from '../../service/api';
+
+const Component = styled(Box)`
+  height: 70vh;
+  width: 90vh;
 `;
-const Wrapper =styled(Box)`
-display:flex;
-flex-direction:column;
-padding:25px 35px;
-flex:1;
-& > div &,Button, &> p {
-  margin-top:20px;
-}
+
+const Image = styled(Box)`
+  background: #2874f0 url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png) center 85% no-repeat;
+  height: 83%;
+  width: 25%;
+  padding: 45px 35px;
+
+  & > p,
+  & > h5 {
+    color: #ffffff;
+    font-weight: 600;
+  }
 `;
+
+const Wrapper = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  padding: 25px 35px;
+  flex: 1;
+
+  & > * {
+    margin-top: 20px;
+  }
+`;
+
 const LoginButton = styled(Button)`
-text-transform:none;
-background:#FB641B;
-color: #FFF;
-height:48px;
-border-radius:2px;
+  text-transform: none;
+  background: #fb641b;
+  color: #fff;
+  height: 48px;
+  border-radius: 2px;
 `;
+
 const RequestOTP = styled(Button)`
-text-transform:none;
-background:#FFF;
-color: #2874f0;
-height:48px;
-border-radius:2px;
-Box-shadow:0 2px 4px 0 rgb(0 0 0/20%);
+  text-transform: none;
+  background: #fff;
+  color: #2874f0;
+  height: 48px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
 `;
-const Text= styled(Typography)`
-font-size:12px;
-color:#878787;
+
+const Text = styled(Typography)`
+  font-size: 12px;
+  color: #878787;
 `;
+
 const CreateAccount = styled(Typography)`
-font-size:14px;
-text-align:center;
-color:#2874f0;
-font-weight:600;
-cursor:pointer;
+  font-size: 14px;
+  text-align: center;
+  color: #2874f0;
+  font-weight: 600;
+  cursor: pointer;
 `;
-const accountIntialValues ={
+
+const accountInitialValues = {
   login: {
-    view:'login',
+    view: 'login',
     heading: 'Login',
-    subheading:'Get access to your Orders, Wishlist and Recommendations'
+    subheading: 'Get access to your Orders, Wishlist and Recommendations'
   },
-  signup:{
+  signup: {
     view: 'signup',
-    heading: "Looks like you'r new here!",
-    subheading:'Sign up with your mobile number to get started'
+    heading: "Looks like you're new here!",
+    subheading: 'Sign up with your mobile number to get started'
   },
-}
-const signInitialValues ={
+};
+
+const signupInitialValues = {
   firstname: '',
   lastname: '',
   username: '',
   email: '',
   password: '',
-}
-const LoginDialog =({open, setOpen}) => {
-  const [account, toggleAccount] = useState(accountIntialValues.login)
-  const [signup, setSignup] = useState(signInitialValues);
-  const handleClose =() =>{
-    setOpen(false);
-    toggleAccount(accountIntialValues.login);
-  }
-    const toggleSignup =() => {
-      toggleAccount(accountIntialValues.signup);
-    }
-    const onInputChange =(e) =>{
-   setSignup({...signup, [e.target.name]: e.target.value});
-   
-    }
-    const signupUser = async() =>{
-         await  authenticateSignup(signup);
-    }
-    return (
-        <Dialog open={open} onClose ={handleClose} PaperProps={{ sx: {maxWidth:'unset'}}} >
-          <Component>
-            <Box style ={{display:'flex',height:'100%'}}>
-            <Image>
-              <Typography variant ="h5">{account.heading}</Typography>
-              <Typography style ={{marginTop:20}}>{account.subheading}</Typography>
+  phone: ''
+};
 
-            </Image>
-            {
-              account.view === 'login' ?
-            <Wrapper>
-              <TextField variant ="standard" label ="Enter Email/Mobile number"/>
-              <TextField variant ="standard" label ="Enter Password"/>
-              <Text>By continuing, you agree to Flipkart's Terms of use and Privacy Policy.</Text>
-              <LoginButton>Login</LoginButton>
-              <Typography style={{textAlign:'center'}}>OR</Typography>
-              <RequestOTP>Request OTP</RequestOTP>
-              <CreateAccount onClick ={() => toggleSignup()}> New to Flipkart? Create an acoount</CreateAccount>
-            </Wrapper>
-            :
-            <Wrapper>
-              <TextField variant ="standard" onChange={(e) => onInputChange(e)}name ='firstname' label ="Enter First Name"/>
-              <TextField variant ="standard" onChange={(e) => onInputChange(e)}name ='lastname' label ="Enter Last Name"/>
-              <TextField variant ="standard"onChange={(e) => onInputChange(e)}name ='username' label ="Enter Username"/>
-              <TextField variant ="standard" onChange={(e) => onInputChange(e)}name ='email' label ="Enter Email"/>
-              <TextField variant ="standard" onChange={(e) => onInputChange(e)} name ='password'label ="Enter Password"/>
-              <TextField variant ="standard" onChange={(e) => onInputChange(e)}name ='phone' label ="Enter Phone"/>
-              <LoginButton onClick={()=>signupUser}>Continue</LoginButton>
-              
-              <CreateAccount >Existing? Login</CreateAccount>
-            </Wrapper>
-}
-            </Box>
-          </Component>
-        </Dialog>
-    )
-}
+const LoginDialog = ({ open, setOpen }) => {
+  const [account, setAccount] = useState(accountInitialValues.login);
+  const [signup, setSignup] = useState(signupInitialValues);
+
+  const handleClose = () => {
+    setOpen(false);
+    setAccount(accountInitialValues.login);
+  };
+
+  const toggleSignup = () => {
+    setAccount(accountInitialValues.signup);
+  };
+
+  const onInputChange = (e) => {
+    setSignup({ ...signup, [e.target.name]: e.target.value });
+  };
+
+  const signupUser = async () => {
+    await authenticateSignup(signup);
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose} PaperProps={{ sx: { maxWidth: 'unset' } }}>
+      <Component>
+        <Box style={{ display: 'flex', height: '100%' }}>
+          <Image>
+            <Typography variant="h5">{account.heading}</Typography>
+            <Typography style={{ marginTop: 20 }}>{account.subheading}</Typography>
+          </Image>
+          <Wrapper>
+            {account.view === 'login' ? (
+              <>
+                <TextField variant="standard" label="Enter Email/Mobile number" />
+                <TextField variant="standard" label="Enter Password" />
+                <Text>By continuing, you agree to Flipkart's Terms of use and Privacy Policy.</Text>
+                <LoginButton>Login</LoginButton>
+                <Typography style={{ textAlign: 'center' }}>OR</Typography>
+                <RequestOTP>Request OTP</RequestOTP>
+                <CreateAccount onClick={toggleSignup}>New to Flipkart? Create an account</CreateAccount>
+              </>
+            ) : (
+              <>
+                <TextField variant="standard" onChange={onInputChange} name="firstname" label="Enter First Name" />
+                <TextField variant="standard" onChange={onInputChange} name="lastname" label="Enter Last Name" />
+                <TextField variant="standard" onChange={onInputChange} name="username" label="Enter Username" />
+                <TextField variant="standard" onChange={onInputChange} name="email" label="Enter Email" />
+                <TextField variant="standard" onChange={onInputChange} name="password" label="Enter Password" />
+                <TextField variant="standard" onChange={onInputChange} name="phone" label="Enter Phone" />
+                <LoginButton onClick={signupUser}>Continue</LoginButton>
+                <CreateAccount onClick={() => setAccount(accountInitialValues.login)}>Existing? Login</CreateAccount>
+              </>
+            )}
+          </Wrapper>
+        </Box>
+      </Component>
+    </Dialog>
+  );
+};
+
 export default LoginDialog;
--- src/service/api.js
+++ src/service/api.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
-const URL ='http://localhost:8000';
-export const authenticateSignup =async (data) =>{
 
-    try{
-      return await  axios.post(`${URL}/signup`, data)
-    } catch (error){
-        console.log("Error While calling signup api",error)
+const URL = "http://localhost:8000";
+
+export const authenticateSignup = async (data) => {
+    try {
+        const response = await axios.post(`${URL}/signup`, data);
+        return response.data; // Return the response data directly
+    } catch (error) {
+        console.error("Error while calling signup API:", error); // Log error for debugging
+        throw error; // Re-throw the error to handle it in the component
     }
-}
+};
